538b18166b86362ca651fd0fb029589288a9520c
Mahef23 <monkeygidr0@gmail.com>
2024-10-16 07:05:34 +0300

    1610

--- Marks.c
+++ Marks.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+#include <string.h>
 int main (){
 	int a[5]={1,2,3,5,6};
-	int n=5,i;
+	int n=sizeof(a)/4,i;
 	for(i=0; i<n ; i++){
-	printf("%d \n",a[i]);
-}
+	printf("%d -",a[i]);
+	if (i== (n-1)) a[i] = a[i];
+	else a[i]=a[i]+a[i+1];
+	printf("%d \n" ,a[i]);
+	}
 return 0;
 }
