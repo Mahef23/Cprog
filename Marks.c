b098c62fa0b1f32bfe184f65959012e4f2492592
Mahef23 <monkeygidr0@gmail.com>
2024-10-17 21:31:32 +0300

    1710

--- Marks.c
+++ Marks.c
@@ -2,12 +2,17 @@
 #include <string.h>
 int main (){
 	int a[5]={1,2,3,5,6};
-	int n=sizeof(a)/4,i;
+	int n=sizeof(a)/4,i,j;
 	for(i=0; i<n ; i++){
 	printf("%d -",a[i]);
 	if (i== (n-1)) a[i] = a[i];
 	else a[i]=a[i]+a[i+1];
 	printf("%d \n" ,a[i]);
+	}
+	for(i=0 ; i<n ; i++){
+		for (j=i+1; j<n ; j++){
+			
+		}
 	}
 return 0;
 }
--- marks.c
+++ marks.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <math.h>
 char* palyndrome( char num[]){
 	int i=0, n=strlen(num)-1;
 		for (i=0; i<= n/2 ; i++){
@@ -20,8 +20,24 @@ void marks(int mark){
 	else if (mark>=90 && mark<100)printf("Very Good");
 	else printf("Excellent");
 }
+void reverse(char mot[]){
+	for(int i=strlen(mot)-1; i>=0 ; i--){
+	printf("%c",mot[i]);
+	}
+}
+void prime(int number){
+	int n, val=sqrt(number);
+
+	for(int i=2; i<=val; i++){
+	if(number%i ==0) n++;
+	}
+	if (n!=0) printf("Not Prime");
+	else printf("Prime");
+}
 int main (){
 	//printf("%s",palyndrome("121"));
-	marks(120);
+	//marks(120);
+	//reverse("Paul");
+	prime(23);
 return 0;
 }
