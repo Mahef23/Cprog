491f2d13801a8c365ff49267553cfd9e8e7dff15
Mahef23 <monkeygidr0@gmail.com>
2024-10-18 07:13:42 +0300

    1810

---
+++ pratical1.py
@@ -0,0 +1,28 @@
+#Cuboid
+"""len = 45
+wid = 25
+haut = 16
+
+vol = len*wid*haut
+
+print(f"{vol} cm3")
+"""
+#cube
+"""cote = 5
+vol = cote**3
+
+print(f"{vol} cm3")
+"""
+
+#rectangle
+"""long = 15 
+larg = 10
+area = long*larg
+print(f"{area} cm2")
+"""
+import math
+#circle
+#pi = 22/7
+rayon = 4
+area = math.pi * (rayon**2)
+print(f"{area} cm2")
